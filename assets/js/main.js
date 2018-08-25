9a4cc0a756d6ca46d71b70a24d852336b6560110
unknown <harikrishnan@skiploop.com>
2018-08-26 00:38:48 +0530

    tablet fix added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,81 +1,89 @@
 $(document).ready(function () {
-    $('#fullpage').fullpage({
-        navigation: false,
-        slidesNavigation: false,
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5thpage', '6thpage', '7thpage'],
-        menu: '#menus',
-        lazyLoad: true,
-        afterLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-            switch (index.index) {
-                case 0:
-                    $("#menuHolder").removeAttr('class');
-                    $('#menuHolder').addClass('menus-holder home-bg');
-                    if ($('.menu-icon').hasClass('show-close')) {
-                        $('.menu-icon').removeClass('show-close')
-                    }
-                    break;
-                case 1:
-                    $("#menuHolder").removeAttr('class');
-                    $('#menuHolder').addClass('menus-holder home-bg about-bg');
-                    if ($('.menu-icon').hasClass('show-close')) {
-                        $('.menu-icon').removeClass('show-close')
-                    }
-                    break;
-                case 2:
-                    $("#menuHolder").removeAttr('class');
-                    $('#menuHolder').addClass('menus-holder home-bg services-bg');
-                    if ($('.menu-icon').hasClass('show-close')) {
-                        $('.menu-icon').removeClass('show-close')
-                    }
-                    break;
-                case 3:
-                    $("#menuHolder").removeAttr('class');
-                    $('#menuHolder').addClass('menus-holder home-bg latest-work-bg');
-                    if ($('.menu-icon').hasClass('show-close')) {
-                        $('.menu-icon').removeClass('show-close')
-                    }
-                    break;
-                case 4:
-                    $("#menuHolder").removeAttr('class');
-                    $('#menuHolder').addClass('menus-holder home-bg meet-team-bg');
-                    if ($('.menu-icon').hasClass('show-close')) {
-                        $('.menu-icon').removeClass('show-close')
-                    }
-                    break;
-                case 5:
-                    $("#menuHolder").removeAttr('class');
-                    $('#menuHolder').addClass('menus-holder home-bg business-sectors-bg');
-                    if ($('.menu-icon').hasClass('show-close')) {
-                        $('.menu-icon').removeClass('show-close')
-                    }
-                    break;
-                case 6:
-                    $("#menuHolder").removeAttr('class');
-                    $('#menuHolder').addClass('menus-holder home-bg contact-bg');
-                    if ($('.menu-icon').hasClass('show-close')) {
-                        $('.menu-icon').removeClass('show-close')
-                    }
-                    break;
-                default:
-                    break;
-            }
-        },
-    });
-    var owl = $('.our-team-slider');
+    if (window.visualViewport.width > 1025) {
+        $('#fullpage').fullpage({
+            licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+            navigation: false,
+            slidesNavigation: false,
+            anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5thpage', '6thpage', '7thpage'],
+            menu: '#menus',
+            afterLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+                switch (index.index) {
+                    case 0:
+                        $("#menuHolder").removeAttr('class');
+                        $('#menuHolder').addClass('menus-holder home-bg');
+                        if ($('.menu-icon').hasClass('show-close')) {
+                            $('.menu-icon').removeClass('show-close')
+                        }
+                        break;
+                    case 1:
+                        $("#menuHolder").removeAttr('class');
+                        $('#menuHolder').addClass('menus-holder home-bg about-bg');
+                        if ($('.menu-icon').hasClass('show-close')) {
+                            $('.menu-icon').removeClass('show-close')
+                        }
+                        break;
+                    case 2:
+                        $("#menuHolder").removeAttr('class');
+                        $('#menuHolder').addClass('menus-holder home-bg services-bg');
+                        if ($('.menu-icon').hasClass('show-close')) {
+                            $('.menu-icon').removeClass('show-close')
+                        }
+                        break;
+                    case 3:
+                        $("#menuHolder").removeAttr('class');
+                        $('#menuHolder').addClass('menus-holder home-bg latest-work-bg');
+                        if ($('.menu-icon').hasClass('show-close')) {
+                            $('.menu-icon').removeClass('show-close')
+                        }
+                        break;
+                    case 4:
+                        $("#menuHolder").removeAttr('class');
+                        $('#menuHolder').addClass('menus-holder home-bg meet-team-bg');
+                        if ($('.menu-icon').hasClass('show-close')) {
+                            $('.menu-icon').removeClass('show-close')
+                        }
+                        break;
+                    case 5:
+                        $("#menuHolder").removeAttr('class');
+                        $('#menuHolder').addClass('menus-holder home-bg business-sectors-bg');
+                        if ($('.menu-icon').hasClass('show-close')) {
+                            $('.menu-icon').removeClass('show-close')
+                        }
+                        break;
+                    case 6:
+                        $("#menuHolder").removeAttr('class');
+                        $('#menuHolder').addClass('menus-holder home-bg contact-bg');
+                        if ($('.menu-icon').hasClass('show-close')) {
+                            $('.menu-icon').removeClass('show-close')
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            },
+        }); 
+    }
+    // $('#section2 ul li .content p').ellipsis({ lines: 3 }); 
+    let owl = $('.our-team-slider');
     owl.owlCarousel({
         margin: 10,
         loop: true,
         items: 1,
-        nav: true
+        nav: true,
+        responsive: {
+            768: {
+                items: 1
+            }
+        } 
     });
     owl.find('.owl-prev').html('<i class="fas fa-caret-left"></i>');
     owl.find('.owl-next').html('<i class="fas fa-caret-right"></i>');
-    var menuTrigger = $('.menu-icon');
+    let menuTrigger = $('.menu-icon');
     menuTrigger.click(function () {
         $(this).toggleClass('show-close');
         $('#menuHolder').toggleClass('show-menus');
     });
-    var menuItem = $('.menus li');
+    let menuItem = $('.menus li');
     menuItem.mouseover(function (e) {
         switch (e.target.innerHTML) {
             case 'about':
